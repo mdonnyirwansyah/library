cdcdcad34360dd8484abf763a2fa1443b525e68d
M.Donny <irwansyah.jr27@gmail.com>
2021-11-08 17:06:45 +0700

    fixing buku module

--- resources/views/app/buku/index.blade.php
+++ resources/views/app/buku/index.blade.php
@@ -10,7 +10,7 @@
 @section('content')
 <section class="section">
   <div class="section-header">
-    <h1>buku</h1>
+    <h1>Buku</h1>
     <div class="section-header-breadcrumb">
       <div class="breadcrumb-item active">
         <a href="{{ route('dashboard') }}">Dashboard</a>
