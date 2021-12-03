5bfbb3b7910cec55dea15ccf30f69635d8633d78
Muhammad Donny Irwansyah <irwansyah.jr27@gmail.com>
2021-12-03 20:34:53 +0700

    fixing fontawesome

--- resources/views/layouts/skeleton.blade.php
+++ resources/views/layouts/skeleton.blade.php
@@ -5,7 +5,9 @@
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
   <title>@yield('title', 'Laravel') &mdash; {{ config('app.name') }}</title>
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('css/fontawesome.min.css') }}">
   @stack('stylesheet')
 </head>
 
@@ -13,10 +15,10 @@
 <div id="app">
   @yield('body')
 </div>
-<script src="{{ mix('js/manifest.js') }}"></script>
-<script src="{{ mix('js/vendor.js') }}"></script>
-<script src="{{ mix('js/popper.js') }}"></script>
-<script src="{{ mix('js/app.js') }}"></script>
+<script src="{{ asset('js/manifest.js') }}"></script>
+<script src="{{ asset('js/vendor.js') }}"></script>
+<script src="{{ asset('js/popper.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
 @stack('javascript')
 </body>
 </html>
