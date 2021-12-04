bd9376f117ac132cb47bf99051bf417c4cca9683
Muhammad Donny Irwansyah <irwansyah.jr27@gmail.com>
2021-12-04 21:56:22 +0700

    fixing typo

--- resources/views/app/tahun-pelajaran/index.blade.php
+++ resources/views/app/tahun-pelajaran/index.blade.php
@@ -10,7 +10,7 @@
 @section('content')
 <section class="section">
   <div class="section-header">
-    <h1>tahun-pelajaran</h1>
+    <h1>Tahun Pelajaran</h1>
     <div class="section-header-breadcrumb">
       <div class="breadcrumb-item active">
         <a href="{{ route('dashboard') }}">Dashboard</a>
