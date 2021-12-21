5db9f817899d70eb8843178a37beab44fbefe990
Muhammad Donny Irwansyah <irwansyah.jr27@gmail.com>
2021-12-21 07:38:32 +0700

    fixing typo

--- resources/views/app/peminjaman/show.blade.php
+++ resources/views/app/peminjaman/show.blade.php
@@ -5,11 +5,11 @@
     <div class="col-sm-12 col-md-7">
         <div class="custom-control custom-radio custom-control-inline mt-1">
             <input type="radio" id="{{ $item->judul.'1' }}" name="pengembalian[buku][{{ $item->id }}][status]" class="custom-control-input" value="1">
-            <label class="custom-control-label" for="{{ $item->judul.'1' }}">Ya</label>
+            <label class="custom-control-label" for="{{ $item->judul.'1' }}">Sudah</label>
         </div>
         <div class="custom-control custom-radio custom-control-inline">
             <input type="radio" id="{{ $item->judul.'0' }}" name="pengembalian[buku][{{ $item->id }}][status]" class="custom-control-input" value="0" checked>
-            <label class="custom-control-label" for="{{ $item->judul.'0' }}">Tidak</label>
+            <label class="custom-control-label" for="{{ $item->judul.'0' }}">Belum</label>
         </div>
     </div>
 </div>
